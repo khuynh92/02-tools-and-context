dd8b30ade09c58b0d1ae67baf7f652f5d6877d54
Khoa Huynh <khoawin92@gmail.com>
2018-05-17 23:34:09 -0700

    completed forEach and map and edited md file

--- lab-khoa/__test__/lib/list.spec.js
+++ lab-khoa/__test__/lib/list.spec.js
@@ -27,14 +27,13 @@ describe('List Module', () => {
     expect(newList.pop()).toBeUndefined();
   });
 
-  it('pop() should return the updated length of the list', () => {
+  it('pop() should return the removed value', () => {
 
     let newList = new List();
     newList.push('foo');
     newList.push('bar');
-    newList.pop();
 
-    expect(newList.length).toEqual(1);
+    expect(newList.pop()).toEqual('bar');
   });
 
   it('pop() should remove the last element of the list', () => {
@@ -42,10 +41,58 @@ describe('List Module', () => {
     let newList = new List();
     newList.push('foo');
     newList.push('bar');
-    console.log(newList);
     newList.pop();
-    console.log(newList);
 
     expect(newList[newList.length - 1]).toEqual('foo');
   });
+
+  it('forEach() should return an error message if argument is not a function', () => {
+    let newList =  new List();
+    expect(newList.forEach('hello')).toEqual(`error, hello is not a function`);
+  });
+
+  it('forEach() should return undefined when a function is passed through', () => {
+    let newList = new List();
+    newList.push(1);
+    let expected = newList.forEach(element => console.log(element));
+    expect(expected).toBeUndefined();
+  });
+
+  it('forEach() should apply the function given in as an argument to each element in the list',() => {
+    let newList = new List();
+    newList.push(2);
+    newList.push(10);
+    let listPlusOne = new List();
+    newList.forEach(element => listPlusOne.push(element+1));
+
+    expect(listPlusOne[1]).toEqual(11);
+  });
+
+  it('map() should return an error message if argument is not a function', () => {
+    let newList = new List();
+    expect(newList.map('hello')).toEqual('error, hello is not a function');
+  });
+
+  it('map() should return the new updated list', () => {
+    let newList = new List();
+    newList.push(5);
+    newList.push(10);
+    let expected = newList.map(element => element*2);
+    let actual = `{0: 10, '1': 20, length: 2}`
+    expect(expected).toEqual(actual);
+  });
+
+  it('map() should apply a function to every element in the array', () => {
+    let newList = new List();
+    newList.push(5);
+    newList.push(10);
+    let multList = newList.map(element => element*2);
+
+    
+    expect(multList[1]).toEqual(20);
+  });
+  
+
+
+  
 });
--- lab-khoa/lib/list.js
+++ lab-khoa/lib/list.js
@@ -11,12 +11,35 @@ class List {
   }
 
   pop() {
+    let deleteMe = this[this.length-1];
     this.length--;
     if (this.length < 0) return undefined;
     else {
       delete this[this.length];
     }
-  } 
+    return deleteMe;
+  }
+  
+  forEach(func) {
+    if(typeof func !== 'function') return `error, ${func} is not a function`;
+    else {
+      for(let i = 0; i < this.length; i++) {
+        func(this[i]);
+      }
+      return undefined;
+    }
+  }
+  
+  map(func) {
+    if(typeof func !== 'function') return `error, ${func} is not a function`;
+    else {
+      for(let i = 0; i < this.length; i++) {
+        this[i] = func(this[i]);
+      }
+      console.log(this);
+      return this;
+    }
+  }
 }
 
 module.exports = List;
